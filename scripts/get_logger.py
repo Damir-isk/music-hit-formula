82ede3c1039f7065c4e6e2067bf1df1f571ffa74
Damir <iskulov.damir.ka@gmail.com>
2026-04-07 23:05:34 +0300

    restructure scripts and upload data and logs

---
+++ scripts/get_logger.py
@@ -0,0 +1,39 @@
+import inspect
+import logging
+import logging.config
+from pathlib import Path
+
+
+def get_logger(name: str) -> logging.Logger:
+    directory = Path(__file__).parent.parent / 'logs'
+    directory.mkdir(exist_ok=True)
+    filename = Path(inspect.currentframe().f_back.f_code.co_filename).stem
+    config = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'main': {
+                'format': '%(asctime)s [%(levelname)s]: %(message)s',
+            },
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'level': 'DEBUG',
+                'formatter': 'main',
+            },
+            'file': {
+                'class': 'logging.FileHandler',
+                'level': 'INFO',
+                'formatter': 'main',
+                'filename': str(directory / f'{filename}.log'),
+                'encoding': 'utf8',
+            },
+        },
+        'root': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+        },
+    }
+    logging.config.dictConfig(config)
+    return logging.getLogger(name)
